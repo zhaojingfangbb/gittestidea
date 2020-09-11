8a9a40b61b02957b53a842dc963166bbc1c15633
dyj <dyj@1673.com>
2020-09-11 11:17:52 +0800

    初始化提交

---
+++ src/com/hello.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @program: gittest
+ * @description:
+ * @author: dyj
+ * @create: 2020-09-11 11:17:11
+ **/
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
